151ad9978269948b85fad6e85030c080f73ef212
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-01-26 11:58:41 -0800

    add pics back into src/lib

--- src/routes/dogs/+page.svelte
+++ src/routes/dogs/+page.svelte
@@ -1,41 +1,38 @@
 <script lang="ts">
   // const imagePaths = Object.keys(
   //   import.meta.glob("$lib/assets/dogs/*.{avif,gif,heif,jpeg,jpg,png,tiff,webp}", {
-  //     eager: true
+  //     query: {
+  //       enhanced: true
+  //     }
   //   })
   // );
 
-  // TODO: find a better way to get all images in a folder that works in
-  // production (the above solution doesn't work)
-  const imagePaths = [
-    "assets/dogs/bl1.png",
-    "assets/dogs/bl2.png",
-    "assets/dogs/bl3.png",
-    "assets/dogs/bl4.png",
-    "assets/dogs/bl5.png",
-    "assets/dogs/bu1.png",
-    "assets/dogs/bu2.png",
-    "assets/dogs/bu3.png",
-    "assets/dogs/bu4.png",
-    "assets/dogs/bu5.png",
-    "assets/dogs/bu6.png"
-  ];
-
-  const images = imagePaths.map((path) => {
-    const alt = path.includes("bu") ? "Buster" : "Blueberry";
-    return { path, alt };
+  const images = import.meta.glob("$lib/assets/dogs/*.{avif,gif,heif,jpeg,jpg,png,tiff,webp}", {
+    query: {
+      enhanced: true
+    }
   });
+  const imagePaths = Object.keys(images);
+
+  const getImagePath = async (path: string) => {
+    // @ts-ignore
+    const { default: imagePath } = await images[path]();
+    const alt = path.includes("bu") ? "Buster" : "Blueberry";
+    return { src: imagePath, alt };
+  };
 </script>
 
 <div class="container">
   <h1 class="text-5xl font-bold py-16 text-center">My beautiful dogs: Blueberry and Buster :)</h1>
   <div class="grid grid-cols-1 md:grid-cols-2 px-0 md:px-10">
-    {#each images as image}
-      <img
-        src={image.path}
-        alt={image.alt}
-        class="shadow-xl object-scale-down mx-auto h-auto scale-90 md:scale-75 hover:scale-100 w-full md:w-1/2"
-      />
+    {#each imagePaths as path}
+      {#await getImagePath(path) then imagePath}
+        <enhanced:img
+          src={imagePath.src}
+          alt={imagePath.alt}
+          class="shadow-xl object-scale-down mx-auto h-auto scale-90 md:scale-75 hover:scale-100 w-full md:w-1/2"
+        />
+      {/await}
     {/each}
   </div>
 </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 import Icons from "unplugin-icons/vite";
+import { enhancedImages } from "@sveltejs/enhanced-img";
 
 export default defineConfig({
   plugins: [
@@ -8,6 +9,7 @@ export default defineConfig({
     Icons({
       scale: 2.5,
       compiler: "svelte"
-    })
+    }),
+    enhancedImages()
   ]
 });
