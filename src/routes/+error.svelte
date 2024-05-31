2dca8942dfcce24ebc180c51cd66276aca6e238e
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-05-31 11:47:05 -0700

    update error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -10,16 +10,18 @@
   <div class="hero min-h-screen bg-base-200">
     <div class="hero-content text-center">
       <div class="max-w-md">
-        <h1 class="text-5xl font-bold pb-10 text-center lg:pb-20">{$page.status} - {$page.error?.message}</h1>
+        <h1 class="text-5xl font-bold pb-10 text-center lg:pb-20">
+          {$page.status} - {$page.error?.message}
+        </h1>
         <img
           src="/assets/chopper.gif"
           class="scale-100 pb-10 lg:scale-125 lg:pb-20"
-          alt="404 not found!"
+          alt="Chopper dancing"
         />
         <h2 class="text-4xl font-bold pb-10 text-center lg:pb-15">Go home?</h2>
         <div class="join gap-16 lg:gap-8">
-          <a class="btn btn-primary btn-lg" href="/">Yes</a>
-          <a class="btn btn-primary btn-lg" href="/">YES</a>
+          <a class="btn btn-primary btn-lg" aria-label="Button to go home" href="/">Yes</a>
+          <a class="btn btn-primary btn-lg" aria-label="Button to go home" href="/">YES</a>
         </div>
       </div>
     </div>
