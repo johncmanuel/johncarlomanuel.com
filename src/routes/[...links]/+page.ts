9ae38c24a16fd1b31a1afda69b439f52a46acfc8
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-06-01 23:17:24 -0700

    use universal instead of server

--- src/routes/[...links]/+page.ts
+++ src/routes/[...links]/+page.ts
@@ -1,9 +1,9 @@
 import { error, redirect } from "@sveltejs/kit";
 import Links from "$lib/public/links";
-import type { RequestEvent } from "./$types";
 
-export const load = (req: RequestEvent) => {
+export const load = (req) => {
   const url = req.url;
+  // Remove forward slash
   const routeName = url.pathname.substring(1);
   if (routeName in Links) {
     // @ts-ignore
