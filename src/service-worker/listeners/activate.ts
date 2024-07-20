7d11b780540ffca59e16724f8239c3a64e297ab5
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-07-19 19:28:50 -0700

    run npm format

--- src/service-worker/listeners/activate.ts
+++ src/service-worker/listeners/activate.ts
@@ -6,12 +6,12 @@ import { CACHE } from "../names";
  * Handles the activate event
  */
 export const handleActivate = (event: ExtendableEvent) => {
-    event.waitUntil(deleteOldCaches());
-}
+  event.waitUntil(deleteOldCaches());
+};
 
 // @ts-ignore
 export const deleteOldCaches = async (): Promise<void> => {
-    for (const key of await caches.keys()) {
-        if (key !== CACHE) await caches.delete(key);
-    }
-}
+  for (const key of await caches.keys()) {
+    if (key !== CACHE) await caches.delete(key);
+  }
+};
--- src/service-worker/listeners/fetch.ts
+++ src/service-worker/listeners/fetch.ts
@@ -6,28 +6,28 @@ import { CACHE, ASSETS } from "../names";
  * Handles the fetch event
  */
 export const handleFetch = (event: FetchEvent): void => {
-    // Ignore other requests except GET and other protected routes
-    if (event.request.method !== 'GET') return;
+  // Ignore other requests except GET and other protected routes
+  if (event.request.method !== "GET") return;
 
-    // @ts-ignore
-    event.respondWith(respond(event));
-}
+  // @ts-ignore
+  event.respondWith(respond(event));
+};
 
 export const respond = async (event: FetchEvent): Promise<Response | undefined> => {
-    const url = new URL(event.request.url);
-    const cache = await caches.open(CACHE);
+  const url = new URL(event.request.url);
+  const cache = await caches.open(CACHE);
 
-    if (ASSETS.includes(url.pathname)) {
-        return cache.match(event.request);
-    }
+  if (ASSETS.includes(url.pathname)) {
+    return cache.match(event.request);
+  }
 
-    try {
-        const response = await fetch(event.request);
-        if (response.status === 200) {
-            cache.put(event.request, response.clone());
-        }
-        return response;
-    } catch {
-        return cache.match(event.request);
+  try {
+    const response = await fetch(event.request);
+    if (response.status === 200) {
+      cache.put(event.request, response.clone());
     }
-} 
+    return response;
+  } catch {
+    return cache.match(event.request);
+  }
+};
