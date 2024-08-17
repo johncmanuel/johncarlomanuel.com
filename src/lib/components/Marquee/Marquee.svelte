8366b6c93eed34902dc9cae0947d7925a774dd14
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-08-17 12:29:42 -0700

    update bg on marquee

--- src/lib/components/Marquee/Marquee.svelte
+++ src/lib/components/Marquee/Marquee.svelte
@@ -3,14 +3,14 @@
   export let items: string[] = [];
 </script>
 
-<div class="relative flex whitespace-no-wrap overflow-x-hidden text-sm">
-  <div class="pt-4 animate-marquee whitespace-nowrap">
+<div class="relative flex pt-4 whitespace-no-wrap overflow-x-hidden text-sm">
+  <div class="animate-marquee whitespace-nowrap bg-yellow-1 dark:bg-grey-1">
     {#each items as item}
       <span class="mx-1">{item}</span>
     {/each}
   </div>
 
-  <div class="absolute pt-4 animate-marquee2 whitespace-nowrap">
+  <div class="absolute animate-marquee2 whitespace-nowrap bg-yellow-1 dark:bg-grey-1">
     {#each items as item}
       <span class="mx-1">{item}</span>
     {/each}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -253,7 +253,7 @@
     <div id="footer" class="flex flex-col gap-4 max-w-full px-4">
       <aside class="mx-auto">
         <p class="text-center">
-          Copyright © {new Date().getFullYear()} John Carlo Manuel - All rights reserved.
+          Copyright © {date.getFullYear()} John Carlo Manuel - All rights reserved.
         </p>
       </aside>
     </div>
