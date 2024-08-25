8fe958fba6d17ab645aa8c08b126e293b0cfd1d1
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-08-25 10:34:00 -0700

    add 500 resp code

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,19 +3,23 @@
   // import Game from "$lib/game/404Game.svelte";
   import { onMount } from "svelte";
   import Link from "$components/Link/Link.svelte";
+  import ErrorLogos from "$lib/public/errors";
 
-  $: isDesktop = false;
+  // $: isDesktop = false;
 
   // Using TailwindCSS's default breakpoint for medium-sized devices
   // https://tailwindcss.com/docs/responsive-design
-  const minWidthPixels = 768;
+  // const minWidthPixels = 768;
 
+  // Mainly using Sawaratsuki's logos, so give credits.
   const sawaratsuki = "SWARATSUKI";
   const sawaratsukiLink = "https://github.com/SAWARATSUKI/";
   const sawaratsukiLogoRepo = "KawaiiLogos";
 
+  const errorLogoSrc = ErrorLogos[$page.status] ?? ErrorLogos[404];
+
   onMount(() => {
-    isDesktop = window.innerWidth >= minWidthPixels;
+    // isDesktop = window.innerWidth >= minWidthPixels;
   });
 </script>
 
@@ -28,9 +32,9 @@
   class="flex flex-col justify-center gap-8 md:gap-4 items-center w-screen h-screen md:pb-20"
 >
   <img
-    src="/assets/respcodes/404.png"
+    src={errorLogoSrc}
     class="flex max-w-md md:max-w-4xl pb-10 drop-shadow-2xl"
-    alt="Vtuber 404 logo"
+    alt={`Vtuber ${$page.status} logo`}
     title={$page.error?.message}
   />
   <div class="flex flex-col gap-6 md:gap-8 text-lg items-center">
