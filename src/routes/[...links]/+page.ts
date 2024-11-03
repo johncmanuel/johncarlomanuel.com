77560909f2b3f8264c532d2edb1b5a1c98d72f79
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-11-03 15:59:36 -0800

    redirect to github

--- src/routes/[...links]/+page.ts
+++ src/routes/[...links]/+page.ts
@@ -1,22 +1,39 @@
 import { error, redirect } from "@sveltejs/kit";
 import Links from "$lib/public/links";
+import type { PageLoad } from "./$types";
 
-export const load = (req) => {
+export const load: PageLoad = (req) => {
   const url = req.url;
 
   // Remove forward slash
   const routeName = url.pathname.substring(1);
 
+  const github = Links["github"] + routeName;
+
+  // Check if url is valid
+  try {
+    new URL(github);
+  } catch {
+    throw error(404, { message: "Page not found" });
+  }
+
+  // Prioritize defined links first
   if (routeName in Links) {
-    // @ts-ignore
+    // @ts-ignore: ignore type error when reading from JSON
     const link: string = Links[routeName];
     throw redirect(307, link);
   }
+
   // Add some easter eggs. Hehe...
   switch (routeName) {
+    case "404":
+      throw error(404, { message: "Page not found" });
     case "418":
       throw error(418, { message: "I'm a teapot" });
-    default:
-      throw error(404, { message: "Page not found" });
+    case "500":
+      throw error(500, { message: "Internal server error" });
   }
+
+  // Redirect to GitHub
+  throw redirect(307, github);
 };
