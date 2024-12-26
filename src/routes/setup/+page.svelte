74315ea4cfa9348f8d4be37768c998a40c6bd483
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-12-25 16:47:10 -0800

    init

---
+++ src/routes/setup/+page.svelte
@@ -0,0 +1,8 @@
+<div
+  class="flex gap-10 md:gap-4 py-10 px-5 md:px-0 flex-col w-full max-w-fit min-h-screen mx-auto"
+>
+  <h1 class="text-4xl font-bold text-center md:text-left">Setups</h1>
+  <a href="/" class="py-4 md:py-10" aria-label="Button to go back to homepage"
+    >Click me to go back home!</a
+  >
+</div>
